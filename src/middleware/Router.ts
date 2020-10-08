7a7240d26f50562ff639ae8c296f906016d502e6
James Bradlee <james@ihacks.dev>
2020-10-08 21:45:26 +0200

    :100: Converted Router middleware to use SubMiddleware.

--- src/middleware/Router.ts
+++ src/middleware/Router.ts
@@ -1,72 +1,14 @@
 // Imports
 import type { Addon, MiddlewareFn } from "../types.ts";
-import { Middleware } from "../util/Middleware.ts";
-import { runMiddleware } from "../util/runMiddleware.ts";
-import { match } from "../deps.ts";
+import { SubMiddleware } from "../util/SubMiddleware.ts";
 
 export const isProtocolMiddleware: (method: string) => MiddlewareFn = (method: string) => async (req, res, next) => await next(req.method.toLowerCase().trim() === method.toLowerCase().trim());
 
 /**
  * A router object to route different requests.
  */
-export class Router extends Middleware
+export class Router extends SubMiddleware
 {
-	
-	#middleware: Set<Addon> = new Set();
-	
-	/**
-	 * Run the middleware associated with this router.
-	 * @param req The request object.
-	 * @param res The response object.
-	 * @param next The next middleware function.
-	 */
-	public run: Middleware["run"] = async (req, res, next) => {
-		await runMiddleware(this.#middleware, req, res);
-		await next();
-	};
-	
-	private addMiddlewares (end: boolean, path: string | Addon, ...middleware: Addon[]): void
-	{
-		if (typeof path === "string")
-		{
-			const matcher = match(path, { end });
-			const set = new Set<Addon>();
-			middleware.forEach(_ => set.add(_));
-			this.use(async (req, res, next) => {
-				// deno-lint-ignore no-explicit-any
-				const result = matcher(req.url.pathname) as false | any;
-				if (!result)
-				{
-					await next();
-					return;
-				}
-				const pre = req.url.pathname.substring(0, result.path.length);
-				req.url.pathname = req.url.pathname.substring(result.path.length, req.url.pathname.length);
-				for (let key in result.params)
-				{
-					const value = result.params[key];
-					req.params[key] = value;
-				}
-				const d = await runMiddleware(set, req, res);
-				if (d.continue) req.url.pathname = pre + req.url.pathname;
-				console.log(d.continue)
-				await next(d.continue);
-			});
-		} else
-		{
-			[ path, ...middleware ].forEach(_ => this.#middleware.add(_));
-		}
-	}
-	
-	/**
-	 * Add middleware to the application.
-	 * @param middleware The middleware to add to the application.
-	 */
-	public use (path: string | Addon, ...middleware: Addon[]): this
-	{
-		this.addMiddlewares(false, path, ...middleware);
-		return this;
-	}
 	
 	/**
 	 * Accept all methods.
@@ -75,7 +17,7 @@ export class Router extends Middleware
 	 */
 	public all (path: string, ...middleware: Addon[]): this
 	{
-		this.addMiddlewares(true, path, ...middleware);
+		this.use_end(path, ...middleware);
 		return this;
 	}
 	
